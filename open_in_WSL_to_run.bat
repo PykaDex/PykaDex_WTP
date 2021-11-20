db3cbe8b43bf3b9f0086856e68b82abe275354a6
Shellywell123 <benjamin@shellswell.com>
2021-11-20 15:43:37 +0000

    imporved WSL workaround script

--- open_in_WSL_to_run.bat
+++
@@ -1,7 +0,0 @@
-Rem This File is a workaround for running python scripts on GPU
-Rem Using native windows Cmd from WSL
-
-cd C:\\Users\benja\Programming\Python_Projects\PykaDex-Organisation\PykaDex_WTP\
-python3 pytorch_prediction_pokemon.py
-
-TIMEOUT 100
---
+++ pytorch_prediction_pokemon_WSL.py
@@ -0,0 +1,5 @@
+# This File is a workaround for running python scripts on GPU
+# Using native windows Cmd from WSL
+#
+echo "Running WSL GPU workaround..."
+"/mnt/c/Windows/System32/cmd.exe" /c "python3 pytorch_prediction_pokemon.py" 
